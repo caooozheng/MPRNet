ba8fd611e749431a559722e6fc1639a7f7af5500
caooozheng <cz001223@163.com>
2025-11-29 12:46:37 +0800

    修改保存路径

--- train.py
+++ train.py
@@ -49,9 +49,9 @@ class Trainer(object):
         ###-----------end-----------------------###
 
         ###-------------创建输出目录, 保存模型-------------------###
-        # now_str = datetime.now().strftime("%Y%m%d-%H%M%S")
-        # print("the time is: ",now_str)
-        self.model_save_path = os.path.join(args.save_path, args.model_name, args.dataset)
+        now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("the time is: ",now_str)
+        self.model_save_path = os.path.join(args.save_path, args.model_name, now_str, args.dataset)
         if not os.path.exists(self.model_save_path):
             os.makedirs(self.model_save_path)
         ###----------------------end--------------------------###
